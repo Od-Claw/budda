8cb3b3c1e9dfd36fc9055edb23dd60039df8083f
Od-Claw <biung_od@yahoo.com.tw>
2026-05-15 02:19:12 +0800

    3

--- guji_buddha_hall_3d/vite.config.ts
+++ guji_buddha_hall_3d/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/budda/guji_buddha_hall_3d/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/guji_buddha_hall_3d/" : "/",
   server: {
     host: "0.0.0.0",
     port: 5173
